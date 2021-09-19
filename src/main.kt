8ae0588b859e8ef06c6f643f28adbf8868d4cf66
AlbaqamiAziz <67348395+AlbaqamiAziz@users.noreply.github.com>
2021-09-19 14:07:37 +0300

    Initial commit

---
+++ src/main.kt
@@ -0,0 +1,14 @@
+
+fun main(){
+    println("Hello World")
+
+    val number: Int = 5
+    var name: String = "Aziz"
+    name = "Bob"
+
+    print("Say Something: ")
+    val str = readLine()
+
+    print(str)
+
+}
