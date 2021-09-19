1671beefae12b71371aa0b357b33b48fd91de173
AlbaqamiAziz <67348395+AlbaqamiAziz@users.noreply.github.com>
2021-09-19 14:21:49 +0300

    Greeting Program Practice_1
    Create a String variable that holds the following message: 'Hello user, welcome to my program!'
    
    Get the user to enter their name, then replace the word user with the user's name.

---
+++ src/GreetingProgram.kt
@@ -0,0 +1,10 @@
+fun main(){
+    print("Enter your name: ")
+    var name = readLine()
+    println("Hello $name, welcome to my program!")
+
+    var msg = "Hello user, welcome to my program!"
+    println(msg.replace("user","$name"))
+
+
+}
